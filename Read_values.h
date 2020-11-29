21a2a6e365b9af4b19db7b9c350dce5d37951b9a
lavieth <68887484+lavieth@users.noreply.github.com>
2020-11-29 15:35:51 -0600

    Create Read_values.h

---
+++ Read_values.h
@@ -0,0 +1,12 @@
+#ifndef "READ_VALUES_H"
+#define "READ_VALUES_H"
+
+uint8_t read8 (uint16_toffset, uint8_t* array_name); 
+uint16_t read16 (uint16_toffset, uint8_t* array_name);
+uint32_t read32 (uint16_toffset, uint8_t* array_name);
+
+
+
+
+
+#endif
