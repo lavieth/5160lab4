4197b2226a859d7bad0c5303bc2d3bd6875f8150
lavieth <68887484+lavieth@users.noreply.github.com>
2020-11-30 20:22:10 -0600

    Create Read_values.c

---
+++ Read_values.c
@@ -0,0 +1,23 @@
+#include "Read_values.h"
+
+//do we need the switch for Big to little endian function? part b) of 3 seems to ask for it
+
+
+uint8_t read8 (uint16_t offset, uint8_t * array_name)
+{
+  uint8_t temp8 = *(array_name+offset);
+  return temp8;
+}
+uint16_t read16 (uint16_t offset, uint8_t * array_name)
+{
+  uint16_t temp16 = *(array_name+offset);
+  return temp16;
+}
+
+
+uint16_t read32 (uint16_t offset, uint8_t * array_name)
+{
+  uint32_t temp32 = *(array_name+offset);
+  return temp32;
+
+}
